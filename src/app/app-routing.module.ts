49c9a70f7b3f55e0f854817862cd14149c994c46
Toky <herostoky@gmail.com>
2022-04-07 14:42:33 +0300

    Init Routing system & initial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ServerErrorComponent } from './components/server-error/server-error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'server',
+    component: ServerErrorComponent,
+  },
+  {
+    path: '',
+    loadChildren: ()=>
+      import('./components/main-content/page.module').then((m)=>m.PageModule),
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServerErrorComponent } from './components/server-error/server-error.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    ServerErrorComponent,
+    NotFoundComponent,
+    MainContentComponent,
+    LayoutComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
@@ -26,6 +46,14 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     BrowserAnimationsModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : []
+  ],
+  exports: [
+    AppRoutingModule,
+    MainContentComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
