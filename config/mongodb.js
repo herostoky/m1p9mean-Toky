36709e0af9914a80016564d824c056c2abc2f559
herostoky <herostoky@gmail.com>
2025-06-25 11:40:57 +0300

    wip

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,11 +1,10 @@
-const mongoose  = require('mongoose');
-const UserModel = require('../model/userModel');
+const mongoose = require("mongoose");
+const UserModel = require("../model/userModel");
 
 try {
-    mongoose.connect(process.env.DB_URL);
-}
-catch(err) {
-    console.log(err);
+  mongoose.connect(process.env.DB_URL);
+} catch (err) {
+  console.log(err);
 }
 
 exports.UserModel = UserModel.compile(mongoose);
--- model/userModel.js
+++ model/userModel.js
@@ -1,11 +1,11 @@
 exports.compile = (mongoose) => {
-    const userSchema = mongoose.Schema(
-        {
-            name: String
-        },
-        { 
-            collection: 'user'
-        }
-    );
-    return mongoose.model('user', userSchema);
-}
+  const userSchema = mongoose.Schema(
+    {
+      name: String,
+    },
+    {
+      collection: "user",
+    }
+  );
+  return mongoose.model("user", userSchema);
+};
